ae5ec9c0e78105bac52b487ee0f3e07d241476ad
Jason Chan <jasonchan@Jasons-MacBook-Air.local>
2023-12-11 22:55:58 +0000

    comment out countdown edit mode

--- src/components/coundown.tsx
+++ src/components/coundown.tsx
@@ -80,62 +80,61 @@ const CountdownTimer: React.FC = () => {
   const minutes = Math.floor(timeLeft / 60);
   const seconds = timeLeft % 60;
 
+  const editComponent = () => {
+    return
+    {
+      editMode ? (
+        <div>
+          <input
+            type="number"
+            placeholder="Quarter"
+            value={inputQuarter}
+            onChange={(e) => setInputQuarter(e.target.value)}
+          />
+          <input
+            type="number"
+            placeholder="Minutes"
+            value={inputMinutes}
+            onChange={(e) => setInputMinutes(e.target.value)}
+          />
+          <input
+            type="number"
+            placeholder="Seconds"
+            value={inputSeconds}
+            onChange={(e) => setInputSeconds(e.target.value)}
+          />
+          <button onClick={handleSave}>Save</button>
+        </div>
+      ) : (
+        <></>
+      )
+    }
+
+  }
+
   return (
     <div>
       <div>
-        {editMode ? (
-          <div>
-            <input
-              type="number"
-              placeholder="Minutes"
-              value={inputMinutes}
-              onChange={(e) => setInputMinutes(e.target.value)}
-            />
-            :
-            <input
-              type="number"
-              placeholder="Seconds"
-              value={inputSeconds}
-              onChange={(e) => setInputSeconds(e.target.value)}
-            />
-            <button onClick={handleSave}>Save</button>
+        <div className='flex flex-col' onClick={handleEdit}>
+          <div className='w-full border-gray-200 bg-gray-100 font-bold border-b-0 border text-xs leading-none text-center tracking-wide'>
+            Q{currentQuarter}
           </div>
-        ) : (
-          <div className='flex flex-col' onClick={handleEdit}>
-            <div>
-              {editMode ? (
-                <div>
-                  <input
-                    type="number"
-                    placeholder="Quarter"
-                    value={inputQuarter}
-                    onChange={(e) => setInputQuarter(e.target.value)}
-                  />
-                </div>
-              ) : (
-                <div className='w-full border-gray-200 bg-gray-100 font-bold border-b-0 border text-xs leading-none text-center tracking-wide'>
-                  Q{currentQuarter}
-                </div>
-              )}
-            </div>
-            <div className='flex'>
-              <div className='border-gray-200 border w-10 h-10 flex items-center border-e-0'>
-                <div className='w-full text-center text-xl font-bold'>{minutes < 10 ? `0${minutes}` : minutes}</div>
-              </div>
-
-              <div className='border-gray-200 border w-10 h-10 flex items-center'>
-                <div className='w-full text-center text-xl font-bold'>{seconds < 10 ? `0${seconds}` : seconds}</div>
-              </div>
+          <div className='flex'>
+            <div className='border-gray-200 border w-10 h-10 flex items-center border-e-0'>
+              <div className='w-full text-center text-xl font-bold'>{minutes < 10 ? `0${minutes}` : minutes}</div>
             </div>
 
+            <div className='border-gray-200 border w-10 h-10 flex items-center'>
+              <div className='w-full text-center text-xl font-bold'>{seconds < 10 ? `0${seconds}` : seconds}</div>
+            </div>
           </div>
-        )}
+        </div>
       </div>
 
       <div className='text-center border border-gray-200 border-t-0 flex justify-center'>
 
         <button onClick={handleStart}><IoPlayCircleOutline /></button>
-        <button onClick={handleStop}><IoPauseCircleOutline/></button>
+        <button onClick={handleStop}><IoPauseCircleOutline /></button>
         <button onClick={handleresetCountdown}><IoStopCircleOutline /></button>
       </div>
 
